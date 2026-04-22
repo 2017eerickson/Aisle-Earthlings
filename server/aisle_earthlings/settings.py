5bdb109ceb6511525cc28ea60a5ff537c6e36eaf
ericka <2017eerickson@gmail.com>
2026-04-22 17:44:37 -0400

    explictly listed ec2 address

--- server/aisle_earthlings/settings.py
+++ server/aisle_earthlings/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.getenv('DEBUG', 'True'))  # Convert string to boolean
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '34.238.161.160' ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
